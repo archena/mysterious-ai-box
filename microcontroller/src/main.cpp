cb32e985f9a55a0430a3ec64dfb59a58aa1b7d69
archena <archena@users.noreply.github.com>
2019-07-28 20:01:45 +0100

    Better training and use 3 outputs

--- microcontroller/src/main.cpp
+++ microcontroller/src/main.cpp
@@ -1,132 +1,183 @@
 #include <Arduino.h>
 #include <NeuralNetwork.h>
 
-#define LED_TRAINING 5
-#define MODE_SWITCH 13
-#define TRAIN_STORE_BUTTON 12
-#define TRAIN_INPUT_A 18
-#define TRAIN_INPUT_B 19
-#define TRAIN_OUTPUT 21
-#define NN_OUTPUT_OFF 2
-#define NN_OUTPUT_ON 4
-#define NN_INPUT_A 22
-#define NN_INPUT_B 23
+#define LED_TRAINING_PROGRESS 5
+#define LED_TRAIN_MODE_ENABLED 23
+#define TRAINING_MODE_SWITCH 13
+#define INPUT_A 18
+#define INPUT_B 19
+#define INPUT_C 21
+#define OUTPUT_RED 2
+#define OUTPUT_GREEN 15
+#define OUTPUT_BLUE 4
+#define TRAINING_RED 33
+#define TRAINING_GREEN 32
+#define TRAINING_BLUE 35
 
 #define MODE_EVALUATE 0
-#define MODE_TRAIN 1
+#define MODE_TRAINING 1
 
 int mode = MODE_EVALUATE;
+int trainingChoice = -1;
 
-#define NumberOf(arg) ((unsigned int) (sizeof (arg) / sizeof (arg [0])))
+#define countof(arg) ((unsigned int) (sizeof (arg) / sizeof (arg [0])))
 
-const unsigned int layers[] = {2, 4, 1};
+const unsigned int layers[] = {3, 6, 2};
 
-const float inputs[4][2] = {
+const float inputs[8][3] = {
+  {0, 0, 0},
+  {0, 0, 1},
+  {0, 1, 0},
+  {0, 1, 1},
+  {1, 0, 0},
+  {1, 0, 1},
+  {1, 1, 0},
+  {1, 1, 1}
+};
+
+float expectedOutput[8][2] = {
   {0, 0},
   {0, 1},
   {1, 0},
-  {1, 1}
+  {1, 1},
+  {1, 1},
+  {0, 1},
+  {1, 0},
+  {0, 1},
 };
 
-float expectedOutput[4][1] = {
-  {0},
-  {1},
-  {1},
-  {0}
-};
+NeuralNetwork NN(layers, countof(layers));
 
-NeuralNetwork NN(layers, NumberOf(layers));
+void updateOutput(const float* classification) {
+  if (classification == NULL) {
+    Serial.println("Error: classification is NULL");
+  } else {
+    Serial.printf("Input {%d, %d, %d}\n", digitalRead(INPUT_A), digitalRead(INPUT_B), digitalRead(INPUT_C));
+    Serial.printf("Classification {%f, %f}\n", classification[0], classification[1]);
+
+    bool p = classification[0] >= 0.5 ? true : false;
+    bool q = classification[1] >= 0.5 ? true : false;
+
+    Serial.printf("Decision {%d%d}\n", p, q);
+
+    if (!p && !q) {
+      digitalWrite(OUTPUT_RED, LOW);
+      digitalWrite(OUTPUT_BLUE, LOW);
+      digitalWrite(OUTPUT_GREEN, LOW);
+    } else if (!p && q) {
+      digitalWrite(OUTPUT_RED, HIGH);
+      digitalWrite(OUTPUT_BLUE, LOW);
+      digitalWrite(OUTPUT_GREEN, LOW);
+    } else if (p && !q) {
+      digitalWrite(OUTPUT_RED, LOW);
+      digitalWrite(OUTPUT_BLUE, HIGH);
+      digitalWrite(OUTPUT_GREEN, LOW);
+    } else if (p && q) {
+      digitalWrite(OUTPUT_RED, LOW);
+      digitalWrite(OUTPUT_BLUE, LOW);
+      digitalWrite(OUTPUT_GREEN, HIGH);
+    }
+  }
+}
 
 void train() {
-  digitalWrite(LED_TRAINING, HIGH);
+  digitalWrite(LED_TRAINING_PROGRESS, HIGH);
 
-  // Train a neural network
   const int epochs = 3000;
   for (int i = 0; i < epochs; ++i) {
-    for (int j = 0; j < NumberOf(inputs); ++j) {
+    for (int j = 0; j < countof(inputs); ++j) {
       NN.FeedForward(inputs[j]);
       NN.BackProp(expectedOutput[j]);
     }
   }
 
-  // Evaluate all the inputs and output to serial
-  for (int i = 0; i < NumberOf(inputs); ++i) {
-    float* outputs = NN.FeedForward(inputs[i]);
-    Serial.println(outputs[0], 7);
+  for (int i = 0; i < countof(inputs); ++i) {
+    const float* outputs = NN.FeedForward(inputs[i]);
+    updateOutput(outputs);
+    delay(100);
   }
 
-  // Output weights to serial
   NN.print();
-  digitalWrite(LED_TRAINING, LOW);
+
+  digitalWrite(LED_TRAINING_PROGRESS, LOW);
+}
+
+void reset() {
+  for (int i = 0; i < countof(expectedOutput); ++i) {
+    for (int j = 0; j < countof(expectedOutput[i]); ++j) {
+      expectedOutput[i][j] = 0;
+    }
+  }
 }
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(NN_OUTPUT_OFF, OUTPUT);
-  pinMode(NN_OUTPUT_ON, OUTPUT);
-  pinMode(LED_TRAINING, OUTPUT);
-  pinMode(MODE_SWITCH, OUTPUT);
-  pinMode(TRAIN_STORE_BUTTON, OUTPUT);
-  pinMode(TRAIN_INPUT_A, INPUT);
-  pinMode(TRAIN_INPUT_B, INPUT);
-  pinMode(TRAIN_OUTPUT, INPUT);
-  pinMode(NN_INPUT_A, INPUT);
-  pinMode(NN_INPUT_B, INPUT);
+  pinMode(LED_TRAINING_PROGRESS, OUTPUT);
+  pinMode(LED_TRAIN_MODE_ENABLED, OUTPUT);
+  pinMode(TRAINING_MODE_SWITCH, INPUT);
+  pinMode(INPUT_A, INPUT);
+  pinMode(INPUT_B, INPUT);
+  pinMode(INPUT_C, INPUT);
+  pinMode(OUTPUT_RED, OUTPUT);
+  pinMode(OUTPUT_GREEN, OUTPUT);
+  pinMode(OUTPUT_BLUE, OUTPUT);
+  pinMode(TRAINING_RED, INPUT);
+  pinMode(TRAINING_GREEN, INPUT);
+  pinMode(TRAINING_BLUE, INPUT);
 
   train();
 }
 
-
 void loop() {
-  digitalWrite(NN_OUTPUT_OFF, LOW);
-  digitalWrite(NN_OUTPUT_ON, LOW);
+  const int a = digitalRead(INPUT_A) == HIGH ?  1 : 0;
+  const int b = digitalRead(INPUT_B) == HIGH ?  1 : 0;
+  const int c = digitalRead(INPUT_C) == HIGH ?  1 : 0;
+  const int input = (a << 2) + (b << 1) + c;
 
-  if (digitalRead(MODE_SWITCH) == HIGH) {
-    mode = MODE_TRAIN;
-
-    if (digitalRead(TRAIN_STORE_BUTTON) == HIGH) {
-      int row;
-      int a = digitalRead(TRAIN_INPUT_A);
-      int b = digitalRead(TRAIN_INPUT_B);
-      if (a == LOW && b == LOW) {
-        row = 0;
-      } else if (a == LOW && b == HIGH) {
-        row = 1;
-      } else if (a == HIGH && b == LOW) {
-        row = 2;
-      } else {
-        row = 3;
-      }
-
-      *expectedOutput[row] = digitalRead(TRAIN_OUTPUT) == HIGH ? 1.0f : 0.0f;
-    }
-  } else {
-    if (mode == MODE_TRAIN) {
+  if (mode == MODE_TRAINING) {
+    if (digitalRead(TRAINING_MODE_SWITCH) == LOW) {
       train();
       mode = MODE_EVALUATE;
+      digitalWrite(LED_TRAIN_MODE_ENABLED, LOW);
+      delay(1000);
+      Serial.println("Evaluation mode enabled");
+      return;
     }
 
-    const float input[2] = {
-      digitalRead(NN_INPUT_A) == HIGH ? 1.0f : 0.0f,
-      digitalRead(NN_INPUT_B) == HIGH ? 1.0f : 0.0f
-    };
-    float* classification = NN.FeedForward(input);
-    if (classification == NULL) {
-      Serial.println("Error: classification is NULL");
-    } else if (classification[0] >= 0.5) {
-      digitalWrite(NN_OUTPUT_OFF, HIGH);
-    } else {
-      digitalWrite(NN_OUTPUT_ON, HIGH);
+    const bool red = digitalRead(TRAINING_RED);
+    const bool green = digitalRead(TRAINING_GREEN);
+    const bool blue = digitalRead(TRAINING_BLUE);
+
+    digitalWrite(OUTPUT_RED, red);
+    digitalWrite(OUTPUT_GREEN, green);
+    digitalWrite(OUTPUT_BLUE, blue);
+
+    if (input > 0) {
+      if (red == HIGH) {
+        expectedOutput[input][0] = 0;
+        expectedOutput[input][1] = 1;
+      } else if (green == HIGH) {
+        expectedOutput[input][0] = 1;
+        expectedOutput[input][1] = 0;
+      } else if (blue == HIGH) {
+        expectedOutput[input][0] = 1;
+        expectedOutput[input][1] = 1;
+      }
     }
+  } else {
+    if (digitalRead(TRAINING_MODE_SWITCH) == HIGH) {
+      reset();
+      mode = MODE_TRAINING;
+      digitalWrite(LED_TRAIN_MODE_ENABLED, HIGH);
+      delay(1000);
+      Serial.println("Training mode enabled");
+      return;
+    }
+
+    const float* classification = NN.FeedForward(inputs[input]);
+    updateOutput(classification);
   }
-
-  Serial.println("--train store--mode--a--b--out--");
-  Serial.println(digitalRead(TRAIN_STORE_BUTTON));
-  Serial.println(digitalRead(MODE_SWITCH));
-  Serial.println(digitalRead(TRAIN_INPUT_A));
-  Serial.println(digitalRead(TRAIN_INPUT_B));
-  Serial.println(digitalRead(TRAIN_OUTPUT));
 
   delay(250);
 }
